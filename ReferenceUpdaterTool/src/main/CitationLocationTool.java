a5a5fec03d9af884d0c6e483d0a9f170bad59923
Adam Eshel <adameshel97@gmail.com>
2023-12-31 17:30:49 -0500

    makes string id findable by id with passing unit tests

--- ReferenceUpdaterTool/src/main/CitationLocationTool.java
+++ ReferenceUpdaterTool/src/main/CitationLocationTool.java
@@ -1,3 +1,4 @@
+package main;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -5,12 +6,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class CitationLocationTool {
-	private Map<Integer, String> hashMap = new HashMap<>();
+	private Map<Integer, String> id_to_citation = new HashMap<>();
+	private Map<String, String> citation_to_string_id = new HashMap<>();
 	
 	public CitationLocationTool() {
 		String filePath = "data/oldRefs2.txt";
+		String referencePath = "data/planetEating.texRefs";
 		try {
 			String text = Files.readString(Paths.get(filePath));
 			Pattern pattern = Pattern.compile("\\[(\\d+)]\\s(.*?)(?=\\[\\d+]|$)", Pattern.DOTALL);
@@ -18,15 +23,58 @@ public class CitationLocationTool {
 	        while (matcher.find()) {
 	            String referenceNumber = matcher.group(1);
 	            String sentence = matcher.group(2);
-	            this.hashMap.put(Integer.valueOf(referenceNumber), sentence.replaceAll("\\s+", "").trim());
+	            this.id_to_citation.put(Integer.valueOf(referenceNumber), sentence.replace("\n", ""));
 	        }
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		String current_article = null;
+		try (BufferedReader br = new BufferedReader(new FileReader(referencePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.length() > 0 && line.charAt(0) == '@') {
+                	String[] split = line.split("\\{");
+                	if (split.length > 1) {
+                		current_article = split[1];
+                        current_article = current_article.substring(0, current_article.length()-1);
+                	}	
+                }
+                else if (line.length() >= 9 && line.substring(0,5).equals("title")) {
+                	String title = line.substring(9);
+                	this.citation_to_string_id.put(title.substring(0, title.length()-3), current_article);                	
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+		
+		System.out.println(this.id_to_citation.get(201));
 	}
 	
 	//TODO: Add Logic to update planetEating.texRefs before returning the output sentence
-	public String find_citation_by_id(Integer id) {
-		return this.hashMap.get(id);
+	private String find_citation_by_id(Integer id) {
+		return this.id_to_citation.get(id);
+	}
+	
+	private class Citation {
+		public String author; 
+		public String title;
+
+		public Citation(String text) {	
+			String[] parts = text.split("(?<!\\..)[.?!]\\s+");
+			this.author = parts[0];
+			this.title = parts[1];
+		}
+	}
+	
+	private String find_title_by_id(Integer id) {
+		Citation citation = new Citation(this.find_citation_by_id(id));
+		return citation.title;
+	}
+	
+	public String find_string_id_by_id(Integer id) {
+		String title = this.find_title_by_id(id);
+		return this.citation_to_string_id.get(title);
 	}
 }
--- ReferenceUpdaterTool/src/test/CitationLocationTests.java
+++ ReferenceUpdaterTool/src/test/CitationLocationTests.java
@@ -1,35 +1,100 @@
+package test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
+import main.CitationLocationTool;
 
 class CitationLocationTests {
 
 	@Test
-	void get_id_33_test() {
+	void get_id_33_test() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
 		CitationLocationTool tool = new CitationLocationTool();
+		Method privateMethod = CitationLocationTool.class.getDeclaredMethod("find_citation_by_id", Integer.class);
+		privateMethod.setAccessible(true); 
 		String citation_33 = "C. A. Becker, R. J. Collier, and A. E. Stone. Invited review: Physiological and behavioral "
 				+ "effects of heat stress in dairy cows. Journal of Dairy Science, 103:6751–6770, 2020.";
-		assertEquals(citation_33.replaceAll(" ",  ""), tool.find_citation_by_id(33));
+		String result = (String)privateMethod.invoke(tool, 33);
+		assertEquals(citation_33, result);
 	}
 	
 	@Test
-	void get_id_20_test() {
+	void get_id_20_test() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
 		CitationLocationTool tool = new CitationLocationTool();
-		String citation_20 = "D.J.Augustine,J.D.Derner,L.M.Porensky,H.Wilmer,M.Fernandez-Gimenez,andD.D.Briske.Adaptive, multi-paddock, "
-				+ "rotational grazing management: An experimental, ranch-scale assessment of effects on multiple ecosystem services. "
-				+ "In Proceedings of the XXIV International Grassland Congress / XI International Rangeland Congress "
-				+ "(Sustainable Use of Grassland and Rangeland Resources for Improved Livelihoods), pages 1-4, Nairobi, Kenya, 2021."
-				+ " Kenya Agricultural and Livestock Research Organization.";
-		assertEquals(citation_20.replaceAll(" ", ""), tool.find_citation_by_id(20));
+		Method privateMethod = CitationLocationTool.class.getDeclaredMethod("find_citation_by_id", Integer.class);
+		privateMethod.setAccessible(true); 
+		String citation_20 = "D.J. Augustine, J.D. Derner, L.M. Porensky, H.Wilmer, M.Fernandez-Gimenez, "
+				+ "and D.D. Briske. Adaptive, multi-paddock, rotational grazing management: An experimental, "
+				+ "ranch-scale assessment of effects on multiple ecosystem services. In Proceedings of the "
+				+ "XXIV International Grassland Congress / XI International Rangeland Congress "
+				+ "(Sustainable Use of Grassland and Rangeland Resources for Improved Livelihoods), "
+				+ "pages 1-4, Nairobi, Kenya, 2021. Kenya Agricultural and Livestock Research Organization.";
+		String result = (String)privateMethod.invoke(tool, 20);
+		assertEquals(citation_20, result);
 	}
 	
+	@Test
+	void get_id_7_test() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+		CitationLocationTool tool = new CitationLocationTool();
+		Method privateMethod = CitationLocationTool.class.getDeclaredMethod("find_citation_by_id", Integer.class);
+		privateMethod.setAccessible(true); 
+		String citation_7 = "Tatsuro Amano, Nicola Gerrett, Yoshimitsu Inoue, Takeshi Nishiyasu, "
+				+ "George Havenith, and Narihiko Kondo. "
+				+ "Determination of the maximum rate of eccrine sweat glands’ ion reabsorption using the galvanic skin conductance to local sweat rate relationship. "
+				+ "European Journal of Applied Physiology, 116, 2016.";
+		String result = (String)privateMethod.invoke(tool, 7);
+		assertEquals(citation_7, result);
+	}
 	
 	@Test
-	void get_id_7_test() {
+	void get_title_187_test() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
 		CitationLocationTool tool = new CitationLocationTool();
-		String citation_7 = "Tatsuro Amano, Nicola Gerrett, Yoshimitsu Inoue, Takeshi Nishiyasu, George Havenith, and Narihiko Kondo. Determination of the "
-				+ "maximum rate of eccrine sweat glands’ ion reabsorp tion using the galvanic skin conductance to local sweat rate relationship. European Journal of"
-				+ "Applied Physiology, 116, 2016.";
-		assertEquals(citation_7.replaceAll(" ",  ""), tool.find_citation_by_id(7));
+		Method privateMethod = CitationLocationTool.class.getDeclaredMethod("find_title_by_id", Integer.class);
+		privateMethod.setAccessible(true); 
+		String citation_187_title = "Meat consumption, health, and the environment";
+		String result = (String)privateMethod.invoke(tool, 187);
+		assertEquals(citation_187_title, result);
+	}
+	
+	@Test
+	void get_title_190_test() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+		CitationLocationTool tool = new CitationLocationTool();
+		Method privateMethod = CitationLocationTool.class.getDeclaredMethod("find_title_by_id", Integer.class);
+		privateMethod.setAccessible(true); 
+		String citation_190_title = "Evolution as fact, theory, and path";
+		String result = (String)privateMethod.invoke(tool, 190);
+		assertEquals(citation_190_title, result);
+	}
+	
+	@Test
+	void get_title_193_test() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+		CitationLocationTool tool = new CitationLocationTool();
+		Method privateMethod = CitationLocationTool.class.getDeclaredMethod("find_title_by_id", Integer.class);
+		privateMethod.setAccessible(true); 
+		String citation_193_title = "Meta-analysis of randomized controlled trials of red meat consumption in comparison with various comparison diets on cardiovascular risk factors";
+		String result = (String)privateMethod.invoke(tool, 193);
+		assertEquals(citation_193_title, result);
+	}
+	
+	@Test 
+	void get_string_id_196_test() {
+		CitationLocationTool tool = new CitationLocationTool();
+		String string_id_196 = "HammarEtAl2022";
+		assertEquals(string_id_196, tool.find_string_id_by_id(196));
+	}
+	
+	@Test 
+	void get_string_id_199_test() {
+		CitationLocationTool tool = new CitationLocationTool();
+		String string_id_199 = "HarmoneyJaeger2018";
+		assertEquals(string_id_199, tool.find_string_id_by_id(199));
+	}
+	
+	@Test 
+	void get_string_id_201_test() {
+		CitationLocationTool tool = new CitationLocationTool();
+		String string_id_201 = "HawkinsEtAl2022";
+		assertEquals(string_id_201, tool.find_string_id_by_id(201));
 	}
 }
